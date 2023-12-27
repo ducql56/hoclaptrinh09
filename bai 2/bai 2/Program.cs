72741633b184783444c11a14abc5e3a7e8326ae4
ducql56 <longduc91193@gmail.com>
2023-12-27 23:44:30 +0700

    update bai1

---
+++ bai 2/bai 2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace bai_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hoten;
+            float DiemToan;
+            float Diemvan;
+            Console.WriteLine(" moi nhap ho ten");
+            hoten = Console.ReadLine();
+            Console.WriteLine("moi nhap diem toan");
+            DiemToan = float.Parse(Console.ReadLine());
+            Console.WriteLine("moi nhap diem van");
+            Diemvan = float.Parse(Console.ReadLine());
+            Console.WriteLine("hoc sinh co diem la:{0},{1},{2}",hoten,DiemToan,Diemvan);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ bai 2/bai3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace bai3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            var y = 338;
+
+            Console.WriteLine("kieu du lieu bien y: {0}",y.GetType().ToString());
+            var z = "duc";
+            Console.WriteLine("kieu du lieu cua z: {0}", z.GetType().ToString());
+
+            Console.ReadKey();
+
+        }
+    }
+}
